39cffe1db21c3ccff431d56fa0abe7fb527071d7
vagrant <vagrant@kleer-csd-box>
2017-07-07 14:23:29 -0300

    rspec funcionando

--- lib/ppt.rb
+++ lib/ppt.rb
@@ -1,18 +1,22 @@
 def get_computer_choice
 	random_number = Random.rand 1..3
-
+		
+	
 	if random_number == 1
 		computer_choice = "piedra"
-	elsif randum_number == 2
+	end
+	if random_number == 2
 		computer_choice = "papel"
-	elsif random_number == 3
+	end
+	if random_number == 3
 		computer_choice = "tijeras"	
 	end
 	
 	return computer_choice
 end
 
-
+=begin
+begin
 def determine_winner (user_choice, computer_choice)
 
   if user_choice == computer_choice
@@ -33,6 +37,7 @@ def determine_winner (user_choice, computer_choice)
 	else
 		return "Ganás vos."
 end
+=end
 
 
 
--- spec/ppt_spec.rb
+++ spec/ppt_spec.rb
@@ -1,3 +1,9 @@
+require './lib/ppt'
 describe "PC elige" do
+
+	it "obtener eleccion pc" do
+		eleccion = get_computer_choice
+		expect(eleccion).not_to eq ""
+	end
 	
 end
